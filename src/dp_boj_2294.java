0aa72988bb20fdfa56bedd79baffb1f2cbf78e98
Myeonghwi <ddkddk35@gmail.com>
2019-03-05 12:46:09 +0900

    동전2 다시풀기

--- src/dp_boj_2294.java
+++ src/dp_boj_2294.java
@@ -8,17 +8,20 @@ public class dp_boj_2294 {
         int coin[] = new int[N + 1];
         int dp[] = new int[K + 1];
 
-        Arrays.fill(dp,100001);
+        Arrays.fill(dp, 100001);
         dp[0] = 0;
 
-        for(int i = 1; i <= N; i++)
+        for (int i = 1; i <= N; i++)
             coin[i] = sc.nextInt();
 
-        for(int i = 1; i <= N; i++) {
-            for(int j = coin[i]; j <= K; j++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = coin[i]; j <= K; j++) {
                 dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
             }
         }
+
+        if (dp[K] == 100001)
+            dp[K] = -1;
 
         System.out.println(dp[K]);
 
